89926248dee52b814192fa8e03f911f837993f24
chimolirgb <chimolilucy@gmail.com>
2021-05-29 10:22:08 +0300

    modified:user.py

---
+++ user.py
@@ -0,0 +1,29 @@
+# Global variables
+global users_list
+class Users:
+    '''
+
+    Class that generates new instances of users
+
+    '''
+   user_list = [] # Empty user list
+
+   def __init__(self,user_name,password): 
+
+       '''
+
+       __init__ method that helps us define properties for our users.
+
+       '''
+
+       self.user_name = user_name
+       self.password = password
+
+      def save_user(self):
+
+          '''
+          save_user method saves user objects into user_list
+
+          ''' 
+
+          Users.user-list.append(self)
